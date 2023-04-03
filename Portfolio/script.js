a4fff67d1f76590e277907b7529da9fe8590ec7e
kaikiwilson <kaikiwilson7@gmail.com>
2023-04-03 01:16:47 -0300

    Professional portfolio made by me

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "FullStack Studant", "Professional of TI"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
