856e4fc97265979d71b3b9bc14a43099ba2e0804
oKai <kaikiwilson7@gmail.com>
2024-01-08 23:07:08 -0300

    Melhoria nos links e visual

---
+++ Portfolio/assets/js/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Desenvolvedor Front-End", "Graduando ADS", "Estudante Full Stack", "Estudante de UI/UX Design", "Profissional de TI"],
+    typeSpeed: 75,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
--- Portfolio/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "FullStack Studant", "Professional of TI"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
