58e92f250342f47d7a0de858a79b1bd92d34f676
BravaKaiki <kaiki.lima@bravabrazil.com.br>
2024-01-26 08:43:30 -0300

    Pequenas modificações no layout

--- Portfolio/assets/js/about.js
+++ Portfolio/assets/js/about.js
@@ -11,25 +11,26 @@ const box_bottom_content = window.document.getElementById("box-bottom-content-id
 const explorer = () => {
     
     const animations = () => {
-        box_h1_princ.style.animation = ' 3s top-slide-out'
-        box_first_h1.style.animation = '3s left-slide-out'
-        box_who_i_am.style.animation = '3s right-slide-out'
-        box_bottom_content.style.animation = '3s top-slide-out'
-        btn.style.animation = '3s fade-out'
+        box_h1_princ.style.animation = '2.5s top-slide-out'
+        box_first_h1.style.animation = '2s left-slide-out'
+        box_who_i_am.style.animation = '2s right-slide-out'
+        box_bottom_content.style.animation = '2.5s top-slide-out'
+        btn.style.animation = '1.25s fade-out'
     }
 
-    const set_display_none = () => {
+    const set_display = () => {
         hard_skills_text.style.display = 'block'
-        container.style.display = 'grid'
+        container.style.display = 'flex'
        
         box_h1_princ.style.display = 'none'
         box_first_h1.style.display = 'none'
         box_who_i_am.style.display = 'none'
         box_bottom_content.style.display = 'none'
-        btn.style.display = '3s fade-out'
+        btn.style.display = 'none'
     }
 
     animations();
+    set_display()
 
 
 
