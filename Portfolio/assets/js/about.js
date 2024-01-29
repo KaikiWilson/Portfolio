f63422aec71bfdbeec2b33d737849bbb61426490
BravaKaiki <kaiki.lima@bravabrazil.com.br>
2024-01-29 14:29:32 -0300

    Add novos conteudos em about me e skills

--- Portfolio/assets/js/about.js
+++ Portfolio/assets/js/about.js
@@ -1,6 +1,4 @@
 const btn = window.document.getElementById("btn-explorer");
-const container = window.document.getElementById("container-id");
-const hard_skills_text = window.document.getElementById("hard-skills");
 
 const box_h1_princ = window.document.getElementById("box-h1-princ-id");
 const box_first_h1 = window.document.getElementById("box-first-h1-id");
@@ -9,29 +7,26 @@ const box_bottom_content = window.document.getElementById("box-bottom-content-id
 
 
 const explorer = () => {
-    
-    const animations = () => {
-        box_h1_princ.style.animation = '2.5s top-slide-out'
-        box_first_h1.style.animation = '2s left-slide-out'
-        box_who_i_am.style.animation = '2s right-slide-out'
-        box_bottom_content.style.animation = '2.5s top-slide-out'
-        btn.style.animation = '1.25s fade-out'
-    }
 
     const set_display = () => {
-        hard_skills_text.style.display = 'block'
-        container.style.display = 'flex'
-       
         box_h1_princ.style.display = 'none'
         box_first_h1.style.display = 'none'
         box_who_i_am.style.display = 'none'
         box_bottom_content.style.display = 'none'
         btn.style.display = 'none'
     }
+    
+    const animations = () => {
+        box_h1_princ.style.animation = '1.75s top-slide-out'
+        box_first_h1.style.animation = '1.5s left-slide-out'
+        box_who_i_am.style.animation = '1.5s right-slide-out'
+        box_bottom_content.style.animation = '1.75s top-slide-out'
+        btn.style.animation = '1.25s fade-out'
+
+        setTimeout(set_display, 1750)
+    }
 
     animations();
-    set_display()
-
 
 
 
---
+++ Portfolio/assets/js/skills.js
@@ -0,0 +1,7 @@
+const container = window.document.getElementById("container-id");
+const hard_skills_text = window.document.getElementById("hard-skills");
+const card = window.document.querySelector(".container");
+
+
+hard_skills_text.style.animation = '1s top-slide-in'
+card.style.animation = '1.25s fade-in'
