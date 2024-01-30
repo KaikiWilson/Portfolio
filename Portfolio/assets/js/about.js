c212b6ea3fc4ba4a68333605e4718ad111ee453f
BravaKaiki <kaiki.lima@bravabrazil.com.br>
2024-01-30 11:30:39 -0300

    style do about me

--- Portfolio/assets/js/about.js
+++ Portfolio/assets/js/about.js
@@ -1,12 +1,29 @@
-const btn = window.document.getElementById("btn-explorer");
-
-const box_h1_princ = window.document.getElementById("box-h1-princ-id");
-const box_first_h1 = window.document.getElementById("box-first-h1-id");
-const box_who_i_am = window.document.getElementById("box-who-i-am-id");
-const box_bottom_content = window.document.getElementById("box-bottom-content-id");
-
-
 const explorer = () => {
+    const box_h1_princ = window.document.getElementById("box-h1-princ-id");
+    const box_first_h1 = window.document.getElementById("box-first-h1-id");
+    const box_who_i_am = window.document.getElementById("box-who-i-am-id");
+    const box_bottom_content = window.document.getElementById("box-bottom-content-id");
+    const btn = window.document.getElementById("btn-explorer");
+
+    const title = window.document.getElementById("about-me-title-id");
+    const subtitle = window.document.getElementById("about-me-subtitle-id");
+    const box_about_me_content = window.document.getElementById("box-about-me-content-id");
+    const container = window.document.getElementById("container-id");
+
+    const show_content = () => {
+        if (btn.style.display === "none") {
+            title.style.display = 'block'
+            subtitle.style.display = 'block'
+            box_about_me_content.style.display = 'flex'
+            container.style.display = 'flex'
+
+            title.style.animation = '1.25s top-slide-in'
+            subtitle.style.animation = '1.25s fade-in'
+            box_about_me_content.style.animation = '1.25s bottom-slide-in'
+            container.style.animation = '2.25s bottom-slide-in'
+        }
+
+    }
 
     const set_display = () => {
         box_h1_princ.style.display = 'none'
@@ -23,11 +40,10 @@ const explorer = () => {
         box_bottom_content.style.animation = '1.75s top-slide-out'
         btn.style.animation = '1.25s fade-out'
 
-        setTimeout(set_display, 1750)
+        setTimeout(set_display, 1740)
     }
 
     animations();
 
-
-
-}
+    setTimeout(show_content, 1750)
+};
